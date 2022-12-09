6ee80f3d805426eeb3f5ceb7a56badd6167b1d27
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-09 01:02:29 -0800

    display all single digit numbers of base 10 in c code

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+ 
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int i;
+	/* display all single digit numbers of base 10 */
+	for (i = 0;i < 10;i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
+
