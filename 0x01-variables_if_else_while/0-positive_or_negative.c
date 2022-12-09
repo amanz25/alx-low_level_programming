5da8cd96380d48c99bb758bf4f27cbcb06fe8c67
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-09 00:41:36 -0800

    display lowercase alphabets in c code

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes here*/
+/* more headers goes there*/
 
 /* betty style doc for function main goes there */
 int main(void)
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* more headers goes there */
+ 
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char i;
+	/* display all lower case characters */
+	for (i = 'a';i <= 'z';i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
+
