d4c72e5cb2cbc31bb31803e738e522d7cb82395d
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-09 00:48:59 -0800

    lower case followed by upper case c code

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,10 +5,16 @@
 int main(void)
 {
 	char i;
+	char j;
 	/* display all lower case alphabets */
 	for (i = 'a';i <= 'z';i++)
 	{
 		putchar(i);
+	}
+	/* followed by all upper case alphabets */
+	for (j = 'A';j <= 'Z';j++)
+	{
+		putchar(j);
 	}
 	putchar('\n');
 	return (0);
