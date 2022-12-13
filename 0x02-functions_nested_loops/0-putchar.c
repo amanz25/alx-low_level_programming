3ec0b2f989169e9d476ec2050a61dc423a83b32b
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-13 04:45:00 -0800

    betty formatted

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,15 +2,15 @@
 
 /**
  * main - program that prints _putchar, followed by a new line
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char stringInArray[] = "_putchar";
-	
+
 	int index;
 
-	for(index = 0; index <= 7; index++)
+	for (index = 0; index <= 7; index++)
 	{
 		_putchar(stringInArray[index]);
 	}
