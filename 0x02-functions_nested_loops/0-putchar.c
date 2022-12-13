533198219e4bdd50108249beb3fdc44f26ac3358
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-13 00:32:52 -0800

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <main.h>
+
+/**
+ * main - program that prints _putchar, followed by a new line
+ * Return : Always 0 (Success)
+ */
+int main(void)
+{
+	char stringInArray[] = "_putchar";
+	
+	int index;
+
+	for(index = 0; index <= 7; index++)
+	{
+		_putchar(stringInArray[index]);
+	}
+
+	_putchar('\n');
+	return (0);
+
+}
