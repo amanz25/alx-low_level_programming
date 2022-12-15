75813fdab9e0df56a369b58309bd9d14f4d1880f
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-15 03:59:12 -0800

    prints the numbers, from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,6 +3,7 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9, followed by a new line
+ *
  * Return: nothing
  */
 
@@ -10,9 +11,9 @@ void print_numbers(void);
 {
 	int d;
 
-	for (d = 0; d <= 9; d++)
+	for (d = 48; d <= 57; d++)
 	{
-		_putchar(d + '0');
+		_putchar(d);
 	}
 	_putchar('\n');
 }
