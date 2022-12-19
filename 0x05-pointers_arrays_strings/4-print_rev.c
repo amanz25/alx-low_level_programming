245e080d0a9b3f804822f5bae431496360ce07ed
Amanuel Kebede <amanuelkebede55@gmail.com>
2022-12-19 09:40:01 -0800

    Update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,18 @@
  */
 void print_rev(char *s)
 {
-	int j = 0;
-	int k;
+	int j;
 
+
+	j = 0;
 	while (s[j] != '\0')
 	{
 		j++;
 	}
 
-	for (k = j ; k >= 0; k--)
+	for (j = j - 1 ; j >= 0; j--)
 	{
-		_putchar(s[k]);
+		_putchar(s[j]);
 	}
 
 	_putchar('\n');
