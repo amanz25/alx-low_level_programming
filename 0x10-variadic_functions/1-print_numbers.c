1babc5c6034657ba6f8c5856730731f73da628bc
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-01-19 11:24:00 -0800

    betty format

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,7 +17,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	while (i < n)
 	{
 		printf("%d", va_arg(ap, int));
-		
 		if (separator != NULL && i != (n - 1))
 			printf("%s", separator);
 	i++;
