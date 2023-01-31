f226f7cc5f989e8b6cdfd39f4066acff37890617
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-01-31 04:05:34 -0800

    update on safe print

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -8,21 +8,72 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count;
+	size_t count, i = 0;
 
-	count = 0;
-	while (head != NULL)
+	count = Check_Unique_Node(head);
+	if (count == 0)
 	{
-		count++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head <= head->next)
+		while (head != NULL)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
-		}
-		else
+			printf("[%p] %d\n", (void *)head, head->n);
 			head = head->next;
+			count++;
+		}
+	}
+	else
+	{
+		while (i < count)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			i++;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
 	}
 
 	return (count);
+}
+
+/**
+ * Check_Unique_Node - check the unique node on the list
+ * @head: pointer to the header
+ *
+ * Return: if there is a loop or not 0 (no loop) else 1
+ */
+size_t Check_Unique_Node(const listint_t *head)
+{
+	size_t count = 1;
+	const listint_t *fast, *slow;
+
+	if (!head || !(head->next))
+		return (0);
+
+	slow = head->next;
+	fast = (head->next)->next;
+
+	while (fast != NULL)
+	{
+		if (fast == slow)
+		{
+			slow = head;
+			while (fast != slow)
+			{
+				count++;
+				slow = slow->next;
+				fast = (fast->next);
+			}
+			slow = slow->next;
+			while (fast != slow)
+			{
+				count++;
+				slow = slow->next;
+			}
+
+			return (count);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+
+	return (0);
 }
