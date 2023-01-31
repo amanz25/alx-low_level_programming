1de85327bf4850487ec582f970434c05e4182b36
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-01-30 22:37:48 -0800

    update on safe print exit with 98

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -18,7 +18,7 @@ size_t print_listint_safe(const listint_t *head)
 		if (head <= head->next)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			exit(98);
 		}
 		else
 			head = head->next;
