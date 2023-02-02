634e7142777b6ff2a3aaf22d4004dd6d3f717bcb
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-02-01 23:36:24 -0800

    function that converts a binary number to an unsigned int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: binary number in string
+ * Return: decimal number or 0 (got failure)
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum, square, num;
+	int k, j, count;
+
+	count = 0;
+	while (b[count] != '\0')
+	{
+		if (b[count] != '0' && b[count] != '1')
+			return (0);
+
+		count++;
+	}
+	sum = 0;
+	for (k = count; k > 0; k--)
+	{
+		square = 1;
+		for (j = 0; j < (count - k); j++)
+			square *= 2;
+
+		if (b[k - 1] == '0')
+			num = 0;
+		else if (b[k - 1] == '1')
+			num = 1;
+		sum += (square * num);
+	}
+
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,7 @@
+#ifndef MATH_H
+#define MATH_H
+
+
+unsigned int binary_to_uint(const char *b);
+
+#endif
