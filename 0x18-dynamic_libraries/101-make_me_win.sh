fe1d2c4bb214b1d66f8a8cc7359dbba7fe33a762
Amanuel Kebede <amanuelkebede55@gmail.com>
2023-03-21 01:21:57 -0700

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P .. https://github.com/amanz25/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmy.so
-export LD_PRELOAD=/tmp/libmy.so
+export LD_PRELOAD=$PWD/libmy.so
